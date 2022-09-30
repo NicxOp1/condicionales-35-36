2ac0855b4e45ed482ea33bc64c2c4899a87522ac
unknown <luxassilva@gmail.com>
2022-09-30 14:06:00 -0300

    ejercicios para los chicos de mern

---
+++ index.js
@@ -0,0 +1,122 @@
+/* console.log("si esta vinculado")
+
+let pregunta = prompt("Como estas?")
+ */
+/* if(pregunta == "bien") {
+    alert("Buenisimo, me alegro por vos")
+    alert("Que te vaya bien, cuidame eso")
+}else if(pregunta == "mal"){
+    alert("bajon")
+}else if(pregunta == "maso"){
+    alert("porque che?")
+} */
+//let num = NaN 
+//if(1) console.log("es true")
+
+/* let mentor = {
+    name: "Lucas",
+    age: 22,
+    heLikesPizza: prompt("Te gusta la pizza?")
+} */
+/* if(mentor.heLikesPizza == "si") {
+    alert("A mi también :D")
+    let otraPregunta = prompt("y que gusto te gusta mas?")
+
+    if(otraPregunta == "muzza") {
+        alert("o mai gad")
+    }else if(otraPregunta == "calabresa"){
+        alert("buena elección chaval")
+    }else{
+        alert("no te entendi, disculpa")
+    }
+
+}else if(mentor.heLikesPizza == "no"){
+    alert("vos te lo perdes")
+}else{
+    alert("disculpa, lo podes volver a repetir")
+} */
+
+//SWITCH CASE
+/* 
+let superheroe = prompt("que superheroe eres")
+
+switch(superheroe){
+    case 'thor':
+        alert("es thor")
+        break
+    case 'superman':
+        alert("es superman")
+        break
+    case 'spiderman':
+        alert("es spiderman")
+        break
+    case 'mateo':
+        alert("es mateo")
+        break
+    default:
+        alert("ninguno")
+} */
+/* 
+let productos = [
+    {
+        nombre: 'Air Force 1',
+        categoria: 'Nike',
+        color: ""
+    },
+    {
+        nombre: 'Bounce',
+        categoria: 'Converse',
+        color: ""
+    },
+    {
+        nombre: 'Yeezy 600',
+        categoria: 'Adidas',
+        color: ""
+    },
+    {
+        nombre: 'Future',
+        categoria: 'Puma',
+        color: ""
+    }
+]
+ */
+/* switch (productos[3].categoria) {
+    case 'Nike':
+        productos[0].color = 'Naranja'
+        console.log(productos[0].color)
+        break
+    case 'Converse':
+        productos[1].color = 'Azul'
+        console.log(productos[1].color)
+        break
+    case 'Adidas':
+        productos[2].color = 'Verde'
+        console.log(productos[2].color)
+        break
+    case 'Puma':
+        productos[3].color = 'Amarillo'
+        console.log(productos[3].color)
+        break
+}
+
+
+
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
